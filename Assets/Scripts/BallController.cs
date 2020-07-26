6442d86150408f56c7c828c68cb4ace15e88634e
Prakhar Sethi <prakhar@playsimple.in>
2020-07-26 14:22:17 +0530

    Fixes in ball jump logic

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
     public float force;
+    private bool addForce;
 
     private Rigidbody2D rb;
 
@@ -15,9 +17,18 @@ public class BallController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate ()
+    void Update ()
     {
         if (Input.GetKeyDown (KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began)) {
+            Debug.Log("Space detected");
+            addForce = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (addForce) {
+            addForce = false;
             rb.AddForce (new Vector2 (0.0f, force));
         }
     }
