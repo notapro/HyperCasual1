189b16f2ee5c8d10a90398ecd360e04e92a4ed0f
Prakhar Sethi <prakhar@playsimple.in>
2020-07-26 14:31:51 +0530

    Added logic for double jump

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,31 +5,61 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public float force;
-    private bool addForce;
-
-    private Rigidbody2D rb;
+    [SerializeField] private float force;
+    [SerializeField] private bool doubleJump;
+    
+    private Rigidbody2D _rigidbody;
+    
+    private bool _addForce;
+    private int _jumpCount;
 
     // Start is called before the first frame update
     void Start ()
     {
-        rb = GetComponent<Rigidbody2D> ();
+        _jumpCount = 0;
+        _rigidbody = GetComponent<Rigidbody2D> ();
     }
 
     // Update is called once per frame
     void Update ()
     {
-        if (Input.GetKeyDown (KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began)) {
+        if (JumpPossible() && Input.GetKeyDown (KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began)) {
             Debug.Log("Space detected");
-            addForce = true;
+            _addForce = true;
+            IncrementJumpCount();
         }
     }
 
     private void FixedUpdate()
     {
-        if (addForce) {
-            addForce = false;
-            rb.AddForce (new Vector2 (0.0f, force));
+        if (_addForce) {
+            _addForce = false;
+            _rigidbody.AddForce (new Vector2 (0.0f, force));
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        ResetJumpCount();
+    }
+
+    private bool JumpPossible()
+    {
+        if (_jumpCount == 0 || (doubleJump && _jumpCount < 2))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void IncrementJumpCount()
+    {
+        _jumpCount++;
+    }
+
+    private void ResetJumpCount()
+    {
+        _jumpCount = 0;
     }
 }
